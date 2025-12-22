782f3d945b20add0b078361300d7c3a4a07efd4f
gmizajalel4-ops <gmizajalel4@gmail.com>
2025-12-22 12:05:11 +0100

    Update SeleniumTestSuite.java
    ggggg

--- selenium-tests2/src/test/java/SeleniumTestSuite.java
+++ selenium-tests2/src/test/java/SeleniumTestSuite.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class SeleniumTestSuite {
 
@@ -23,11 +24,19 @@ public class SeleniumTestSuite {
             new URL("http://localhost:4444/wd/hub"),
             options
         );
+
+        // Set the page load timeout to 5 minutes
+        driver.manage().timeouts().pageLoadTimeout(3, TimeUnit.MINUTES);
+
+        // Set the implicit wait timeout for elements to 30 seconds
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
     public void openHomePage() {
         driver.get("http://192.168.49.2:30080");
+        
+        // Adding a condition to wait for the page to load and assert the title
         Assert.assertTrue(
             driver.getTitle().contains("PetClinic"),
             "Title does not contain PetClinic"

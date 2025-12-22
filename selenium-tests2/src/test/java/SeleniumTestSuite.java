1b60d0e51d3bff37a9adf5a96878cd62f4bab3ae
gmizajalel4-ops <gmizajalel@gmail.com>
2025-12-22 02:37:05 +0100

    Add Selenium tests for PetClinic

---
+++ selenium-tests2/src/test/java/SeleniumTestSuite.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.net.URL;
+
+public class SeleniumTestSuite {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() throws Exception {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+            "--headless",
+            "--no-sandbox",
+            "--disable-dev-shm-usage"
+        );
+
+        driver = new RemoteWebDriver(
+            new URL("http://localhost:4444/wd/hub"),
+            options
+        );
+    }
+
+    @Test
+    public void openHomePage() {
+        driver.get("http://192.168.49.2:30080");
+        Assert.assertTrue(
+            driver.getTitle().contains("PetClinic"),
+            "Title does not contain PetClinic"
+        );
+    }
+
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
